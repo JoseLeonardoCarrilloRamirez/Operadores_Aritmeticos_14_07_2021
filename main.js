98dd23860b0dbd1fa3b6fef9cafa2bf8346c54f1
JoseLeonardoCarrilloRamirez <joseleonardocarrillo@gmail.com>
2021-07-14 21:49:56 -0500

    taller operadores aritmeticos

---
+++ main.js
@@ -0,0 +1,120 @@
+
+//Pedir 2 datos por pantalla y almacenarlo en dos variables 
+//diferentes:
+//Variable 1(num1)  Variable 2 (num2)
+//Realizar todas las Operacion arigameticas vistas en la clase
+//Mostra en la consola todos los posibles resultados
+//Extra: Por medio de una venta emergente(alert) Dar la maxima
+//informacion del desarrallodar del aplicativo en cuestion
+
+let numero1 = prompt `Ingresar numero 1:`;
+let numero2 = prompt `Ingresar numero 2:`;
+
+alert('Para ver los resultados de las operaciones aritmeticas por favor presiones F12');
+
+ console.log(`Numero1 ingresado :`+numero1);
+ console.log(`Numero2 ingresado :`+numero2);
+
+// pre-Incremento
+++numero1;
+numero1++;
+console.log(numero1);
+++numero2;
+numero2++;
+console.log(numero2);
+
+//Decremento
+--numero1;
+numero1--;
+console.log(numero1);
+--numero2;
+numero2--;
+console.log(numero2);
+
+//Division
+let Division = numero1 / numero2;
+console.log(Division);
+
+//Resto
+let resto = numero2 % numero2;
+console.log(resto);
+
+//Exponenciación
+let exp = numero1 ** numero2;
+console.log (exp);
+
+//Multiplicacion
+let multiplicacion = numero1 * numero2;
+console.log(multiplicacion);
+
+//Suma
+let suma = parseInt(numero1) + parseInt(numero2);
+console.log(suma);
+
+//Resta
+let resta = parseInt(numero1) - parseInt(numero2);
+console.log(resta);
+
+//Unario negativo
+console.log(-numero1);
+console.log(-numero2);
+//Unario positivo
+console.log(+numero1);
+console.log(+numero2);
+
+
+//CLASE
+
+// Pre-Incremento
+// 2+numero1;
+// console.log(numero2);
+// 2+numero2;
+// console.log(numero2);
+
+
+// Pos-Incremento
+// let inc = 5;
+// console.log(num);
+// num += inc;
+// console.log(num);
+// num += inc;
+// console.log(num);
+// num += inc;
+// console.log(num);
+// num += inc;
+// console.log(num);
+
+// let num;
+// () Parentesis = Presidente, 0
+// % Modulo = 1
+// * Multiplocacion = 2
+// / Division = 3
+// - Resta = 4
+// + Suma = 5
+// let num = 5;
+
+// // Pre-Incremento
+// 2+num;
+// console.log(num);
+
+// Pos-Incremento
+// let inc = 5;
+// console.log(num);
+// num += inc;
+// console.log(num);
+// num += inc;
+// console.log(num);
+// num += inc;
+// console.log(num);
+// num += inc;
+// console.log(num);
+
+
+// console.log(num++);
+// console.log(num++);
+
+// num++;
+// num++;
+// num++;
+// num++;
+// num++;
